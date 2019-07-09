38efde3640fc516fdbb0d2f01baf3977000d2e40
Felipe <ofelipedbzgt@hotmail.com>
2019-07-09 19:14:33 -0300

    Implemented FHF click

--- clicker.py
+++ clicker.py
@@ -5,27 +5,48 @@ import keyboard
 pyautogui.PAUSE = 0.01
 pyautogui.FAILSAFE = True
 
+big_cookie = (204, 367)
+fhf_up = (570, 535)
+fhf_dn = (570, 671)
 
-def click_loop():
+
+def click_gc_loop():
     last_gc = 0
+    last_fhf = 0
     while True:
-        if keyboard.is_pressed('F4'):
+        if keyboard.is_pressed('ctrl+f3'):
             main_loop()
         else:
-            pyautogui.click(204, 367)
+            pyautogui.click(big_cookie)
             if (time.time() - last_gc) > 135:
                 gc = pyautogui.locateOnScreen('gc2.png', confidence=0.93)
                 if gc is not None:
                     pyautogui.click(pyautogui.center(gc))
-                    time.sleep(1.3)
+                    time.sleep(1)
                     last_gc = time.time()
+            if (time.time() - last_fhf) > 1620:
+                pyautogui.click(fhf_dn)
+                time.sleep(4.5)
+                gc_fhf = pyautogui.locateOnScreen('gc2.png', confidence=0.9)
+                if gc_fhf is not None:
+                    pyautogui.click(pyautogui.center(gc_fhf))
+                last_fhf = time.time()
+                time.sleep(0.5)
+
+
+def click_loop():
+    while True:
+        if keyboard.is_pressed('ctrl+f3'):
+            main_loop()
+        pyautogui.click(204, 367)
 
 
 def main_loop():
     while True:
-        if keyboard.is_pressed('F2'):
+        if keyboard.is_pressed('ctrl+f1'):
             click_loop()
+        elif keyboard.is_pressed('ctrl+f2'):
+            click_gc_loop()
 
 
-time.sleep(0.5)
 main_loop()
---
+++ test.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+
+time.sleep(3)
+
+# fhf up -> x = 570, y = 535
+# fhf dn -> x = 570, y = 671
+
+print(pyautogui.position())
